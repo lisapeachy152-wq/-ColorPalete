9d96639b5aa093b1ba63b2be1f1a0894826912b8
lisapeachy152-wq <lisapeachy152@gmail.com>
2026-06-30 18:31:37 +0100

    Create  config.py

---
+++ config.py
@@ -0,0 +1,13 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+class Config:
+    BOT_TOKEN = os.getenv("BOT_TOKEN")
+    
+    @classmethod
+    def validate(cls):
+        if not cls.BOT_TOKEN:
+            raise ValueError("BOT_TOKEN is not set in environment variables")
+        return True
